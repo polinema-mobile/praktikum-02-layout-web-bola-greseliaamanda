a1c1d3f86bf3b6b214e5001713b8c46eb5e5107f
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 23:12:21 +0700

    percobaan listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,48 @@
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
-//--stack--
+//--listview--
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Stack Widget")),
-        body: Stack(
+        appBar: AppBar(
+          title: Text("Contoh ListView Widget")
+        ),
+        body: ListView(
           children:  <Widget>[
             Container(
               color: Colors.green,
-              alignment:Alignment.bottomCenter,
-              child: Text("Satu", style: TextStyle(fontSize:30,color:
-                Colors.white)),
+              alignment:Alignment.topLeft,
+              child: Text("ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
+                style: TextStyle(
+                  fontSize:30,color:
+                    Colors.white)
+              ),
             ),
             Container(
               color: Colors.red,
-              alignment:Alignment.bottomCenter,
-              child: Text("Dua", style: TextStyle(fontSize:30,color:
-                Colors.white)),
+              alignment:Alignment.topLeft,
+              child: Text("ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
+                style: TextStyle(
+                  fontSize:30,color:
+                    Colors.white)
+              ),
               height: 400.0,
-              width: 300.0,
+              width: 300.0, 
             ),
             Container(
               color: Colors.deepPurple,
-              alignment:Alignment.bottomCenter,
-              child: Text("Tiga", style: TextStyle(fontSize:30,color:
-                Colors.white)),
+              alignment:Alignment.topLeft,
+              child: Text("ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
+                style: TextStyle(
+                  fontSize:30,color:
+                    Colors.white)
+              ),
               height: 200.0,
-              width: 200.0,
+              width: 200.0, 
             ),
           ],
         ),
