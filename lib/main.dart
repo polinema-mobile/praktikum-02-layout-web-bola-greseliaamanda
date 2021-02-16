8bd8e041719b694f7fa5ae397f19b9a44f57766e
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 20:42:27 +0700

    percobaan dialog

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,56 @@ void main() {
   runApp(MyApp());
 }
 
-//--scaffold--
+//--dialog--
 class MyApp extends StatelessWidget {
-  final int _count = 0;
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-          title: Text('Sample Code'),
-          ),
-          body: Center(
-          child: Text('You have pressed the button $_count times.'), 
-          ),
-          bottomNavigationBar: BottomAppBar(
-          child: Container(
-          height: 50.0,
-          ),
-          ),
-          floatingActionButton: FloatingActionButton(
-          onPressed: () => 0,
-          tooltip: 'Increment Counter',
-          child: Icon(Icons.add),
-          ),
-          floatingActionButtonLocation:
-          FloatingActionButtonLocation.centerDocked,
+  //int _count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: Scaffold(
+        body:MyLayout(),
         ),
       );
   }
+}
+    
+class MyLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: RaisedButton(
+      child: Text('Show alert'),
+      onPressed: () {
+      showAlertDialog(context);
+      },
+      ),
+    );
+  }
+}
+
+showAlertDialog(BuildContext context) {
+  // set up the button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () { }, 
+  );
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("My title"),
+    content: Text("This is my message."),
+    actions: [
+    okButton,
+    ],
+  );
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 } 
 
 
