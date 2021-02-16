e0491762621429027652a43499f746c0cadc890a
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 21:58:41 +0700

    percobaan build in layout widget_property transfrm

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,60 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 //--build in layout widget--
-//container -> property padding
+//container -> property transform
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Padding")),
+        appBar: AppBar(title: Text("Contoh Transform")), 
         body: Container(
-          padding: EdgeInsets.only(left: 20),
-          margin: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: NetworkImage(
+                'https://pixnio.com/free-images/2019/09/23/2019-09-23-12-16-56-1200x900.jpg'
+              ),
+              fit: BoxFit.fitWidth,
+            ),
+            border: Border.all(
+              color: Colors.black,
+              width: 8,
+            ),
+          ),
           height: 200,
-          width: 200,
-          alignment: Alignment.topLeft,
-          color: Colors.blueGrey,
-          child: Text(
-            'Ayo Belajar Flutter',
-          style: TextStyle(fontSize: 20, color: Colors.white),
-          )
+          width: 300,
+          margin: const EdgeInsets.only(left: 30.0, right: 30.0,
+          top: 30),
+          transform: Matrix4.rotationZ(-0.1),
         )
       ),
     );
   }
-} 
+}
 
+//container -> property padding
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text("Contoh Padding")),
+//         body: Container(
+//           padding: EdgeInsets.only(left: 20),
+//           margin: EdgeInsets.all(20),
+//           height: 200,
+//           width: 200,
+//           alignment: Alignment.topLeft,
+//           color: Colors.blueGrey,
+//           child: Text(
+//             'Ayo Belajar Flutter',
+//           style: TextStyle(fontSize: 20, color: Colors.white),
+//           )
+//         )
+//       ),
+//     );
+//   }
+// } 
 
 //container -> property margin
 // class MyApp extends StatelessWidget {
