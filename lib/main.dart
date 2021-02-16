bc5a25ebc31e1b34ac9bc5e80427c68788881b4e
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 21:12:01 +0700

    percobaan build in layout widget_property alignmen

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,49 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 //--build in layout widget--
-//container -> property child
+//container -> property alignment
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-      appBar: AppBar(title: Text("Contoh Button")),
-      body: Container(
-        child: RaisedButton(
-          textColor: Colors.white,
-          onPressed: (){},
-          color: Colors.red[900],
+        appBar: AppBar(title: Text("Contoh Aligment")),
+        body: Container(
+          alignment: Alignment.bottomCenter,
           child: Text(
-            "Button",
-          style: TextStyle(fontSize: 20)
-          ),
+            'Semangat Belajar', 
+            style: TextStyle(
+              fontSize: 20,
+              ),
+          )
         )
-      )
       ),
     );
   }
-}
+} 
+
+//container -> property child
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) { 
+//     return MaterialApp(
+//       home: Scaffold(
+//       appBar: AppBar(title: Text("Contoh Button")),
+//       body: Container(
+//         child: RaisedButton(
+//           textColor: Colors.white,
+//           onPressed: (){},
+//           color: Colors.red[900],
+//           child: Text(
+//             "Button",
+//           style: TextStyle(fontSize: 20)
+//           ),
+//         )
+//       )
+//       ),
+//     );
+//   }
+// }
+
+
 
