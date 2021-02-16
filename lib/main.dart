ed29a4f5b3fc75e8d7d70660932e53fa17e630be
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 10:45:17 +0700

    percobaan image widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,24 @@ void main() {
   runApp(MyApp());
 }
 
+//--image widget--
 class MyApp extends StatelessWidget {
 @override
   Widget build(BuildContext context) {
   return MaterialApp(
-  home : Text('This is Text Widget',),
-  ); 
+  home:Image(image: NetworkImage('https://www.shaverscreek.org/wp-content/uploads/2019/02/image3-e1550610760558.jpg'),),
+  );
   }
 } 
+
+//--text widget--
+// class MyApp extends StatelessWidget {
+// @override
+//   Widget build(BuildContext context) {
+//   return MaterialApp(
+//   home : Text('This is Text Widget',),
+//   ); 
+//   }
+// } 
+
+
