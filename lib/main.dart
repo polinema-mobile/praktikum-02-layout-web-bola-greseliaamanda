a8179af20af86d73eff9f91171525110dc010b51
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 22:19:09 +0700

    percobaan build in layout widget_property decrtion

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,66 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 //--build in layout widget--
-//container -> property transform
+//container -> property decoration
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Transform")), 
+        appBar: AppBar(title: Text("Contoh Configuration")),
         body: Container(
           decoration: BoxDecoration(
+            color: const Color(0xff7c94b6),
             image: const DecorationImage(
               image: NetworkImage(
-                'https://pixnio.com/free-images/2019/09/23/2019-09-23-12-16-56-1200x900.jpg'
-              ),
+                'https://pixnio.com/free-images/2019/09/23/2019-09-23-12-16-56-1200x900.jpg'),
               fit: BoxFit.fitWidth,
             ),
             border: Border.all(
               color: Colors.black,
               width: 8,
             ),
+            borderRadius: BorderRadius.circular(12),
           ),
-          height: 200,
-          width: 300,
-          margin: const EdgeInsets.only(left: 30.0, right: 30.0,
-          top: 30),
-          transform: Matrix4.rotationZ(-0.1),
+        height: 200,
+        width: 300,
+        margin: EdgeInsets.all(20)
         )
       ),
     );
-  }
+  } 
 }
+
+//container -> property transform
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text("Contoh Transform")), 
+//         body: Container(
+//           decoration: BoxDecoration(
+//             image: const DecorationImage(
+//               image: NetworkImage(
+//                 'https://pixnio.com/free-images/2019/09/23/2019-09-23-12-16-56-1200x900.jpg'
+//               ),
+//               fit: BoxFit.fitWidth,
+//             ),
+//             border: Border.all(
+//               color: Colors.black,
+//               width: 8,
+//             ),
+//           ),
+//           height: 200,
+//           width: 300,
+//           margin: const EdgeInsets.only(left: 30.0, right: 30.0,
+//           top: 30),
+//           transform: Matrix4.rotationZ(-0.1),
+//         )
+//       ),
+//     );
+//   }
+// }
 
 //container -> property padding
 // class MyApp extends StatelessWidget {
