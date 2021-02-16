d0625b6df0da3555458a2912c384637ba208f372
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 11:07:33 +0700

    percobaan widget image fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,19 @@
-//--ios cupertino--
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
 
-  class MyApp extends StatelessWidget {
-  @override
-    Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        margin: EdgeInsets.only(top: 30),
-        color: Colors.white,
-        child: Column(
-        children: <Widget>[
-          AppBar(title: Text('Contoh Cupertino')),
-          CupertinoButton(
-            child: Text("Contoh button"),
-            onPressed: () {},
-            ),
-          CupertinoActivityIndicator(), 
-        ],
-        ),
-      ),
-    );
-    }
-  }
+void main() {
+  runApp(MyApp());
+}
 
 //--image widget--
-// class MyApp extends StatelessWidget {
-// @override
-//   Widget build(BuildContext context) {
-//   return MaterialApp(
-//   home:Image(image: NetworkImage('https://www.shaverscreek.org/wp-content/uploads/2019/02/image3-e1550610760558.jpg'),),
-//   );
-//   }
-// } 
+class MyApp extends StatelessWidget {
+@override
+  Widget build(BuildContext context) {
+  return MaterialApp(
+  home:Image(image: NetworkImage('https://image.freepik.com/free-vector/cartoon-owl-tree-branch_194935-43.jpg'),
+  ),
+  );
+  }
+} 
 
 //--text widget--
 // class MyApp extends StatelessWidget {
