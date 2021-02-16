3653239a3015c0d34fd1cc0a5b06dd519256e409
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 21:15:39 +0700

    percobaan build in layout widget_property color

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,49 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 //--build in layout widget--
-//container -> property alignment
+//container -> property color
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Aligment")),
+        appBar: AppBar(title: Text("Contoh Color")),
         body: Container(
-          alignment: Alignment.bottomCenter,
+          margin: EdgeInsets.all(50),
+          height: 200,
+          width: 200,
+          alignment: Alignment.center,
+          color: Colors.amber[900],
           child: Text(
-            'Semangat Belajar', 
-            style: TextStyle(
-              fontSize: 20,
-              ),
+            'Semangat Belajar',
+            style: TextStyle(fontSize: 20, color: Colors.white),
           )
         )
       ),
     );
   }
 } 
+
+//container -> property alignment
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text("Contoh Aligment")),
+//         body: Container(
+//           alignment: Alignment.bottomCenter,
+//           child: Text(
+//             'Semangat Belajar', 
+//             style: TextStyle(
+//               fontSize: 20,
+//               ),
+//           )
+//         )
+//       ),
+//     );
+//   }
+// } 
 
 //container -> property child
 // class MyApp extends StatelessWidget {
