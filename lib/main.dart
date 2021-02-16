d9299857d9dd1f76dc23bca401a745958c00bc2a
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 20:29:29 +0700

    percobaan scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,28 @@ class MyApp extends StatelessWidget {
   final int _count = 0;
     @override
     Widget build(BuildContext context) {
-    return MaterialApp(
-    home: Scaffold(
-    appBar: AppBar(
-    title: Text('Sample Code'),
-    ),
-    body: Center(
-    child: Text('You have pressed the button $_count times.'), 
-    ),
-    bottomNavigationBar: BottomAppBar(
-    child: Container(
-    height: 50.0,
-    ),
-    ),
-    floatingActionButton: FloatingActionButton(
-    onPressed: () => 0,
-    tooltip: 'Increment Counter',
-    child: Icon(Icons.add),
-    ),
-    floatingActionButtonLocation:
-    FloatingActionButtonLocation.centerDocked,
-    ),
-    );
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+          title: Text('Sample Code'),
+          ),
+          body: Center(
+          child: Text('You have pressed the button $_count times.'), 
+          ),
+          bottomNavigationBar: BottomAppBar(
+          child: Container(
+          height: 50.0,
+          ),
+          ),
+          floatingActionButton: FloatingActionButton(
+          onPressed: () => 0,
+          tooltip: 'Increment Counter',
+          child: Icon(Icons.add),
+          ),
+          floatingActionButtonLocation:
+          FloatingActionButtonLocation.centerDocked,
+        ),
+      );
   }
 } 
 
