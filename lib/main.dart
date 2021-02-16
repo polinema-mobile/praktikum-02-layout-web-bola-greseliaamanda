aa3961d497a9f01d244afb557002a1a2307e94a3
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 20:58:59 +0700

    percobaan date and time pickers

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,63 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-}
-
-//--input dan selection widget--
+//--date and time pickers--
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Contoh Date Picker',
+      home: MyHomePage(title: 'Contoh Date Picker'),
+    ); 
+ }
+}
 
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Contoh TextField")
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+    _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Variable/State untuk mengambil tanggal
+  DateTime selectedDate = DateTime.now();
+  // Initial SelectDate FLutter
+  Future<Null> _selectDate(BuildContext context) async {
+  // Initial DateTime FIinal Picked
+  final DateTime picked = await showDatePicker(
+    context: context,
+    initialDate: selectedDate,
+    firstDate: DateTime(2015, 8),
+    lastDate: DateTime(2101));
+      if (picked != null && picked != selectedDate)
+        setState(() {
+        selectedDate = picked;
+        });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
         ),
-        body: TextField(
-          obscureText: false,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Nama',
-          ),
-        )
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("${selectedDate.toLocal()}".split(' ')[0]),
+            SizedBox(height: 20.0,),
+            RaisedButton(
+              onPressed: () => {
+                _selectDate(context),
+                print(selectedDate.day + selectedDate.month +
+                selectedDate.year )
+              },
+              child: Text('Pilih Tanggal'),
+            ), 
+          ],
+        ),
       ),
     );
   }
