d83d50bca7e00c000f5d7e4abc8dc5f77839d8b2
greseliaamanda <greseliamanda16@gmail.com>
2021-02-16 20:26:59 +0700

    percobaan scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,71 +4,34 @@ void main() {
   runApp(MyApp());
 }
 
-//--button--
+//--scaffold--
 class MyApp extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
- return MaterialApp(
- home: Scaffold(
- floatingActionButton:FloatingActionButton( 
- onPressed: () {
- // Add your onPressed code here!
- },
- child: Icon(Icons.thumb_up),
- backgroundColor: Colors.pink,
- ),
- ),
- );
- }
+  final int _count = 0;
+    @override
+    Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+    appBar: AppBar(
+    title: Text('Sample Code'),
+    ),
+    body: Center(
+    child: Text('You have pressed the button $_count times.'), 
+    ),
+    bottomNavigationBar: BottomAppBar(
+    child: Container(
+    height: 50.0,
+    ),
+    ),
+    floatingActionButton: FloatingActionButton(
+    onPressed: () => 0,
+    tooltip: 'Increment Counter',
+    child: Icon(Icons.add),
+    ),
+    floatingActionButtonLocation:
+    FloatingActionButtonLocation.centerDocked,
+    ),
+    );
+  }
 } 
-
-
-//--ios cupertino--
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// void main() => runApp(MyApp());
-
-//   class MyApp extends StatelessWidget {
-//   @override
-//     Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Container(
-//         margin: EdgeInsets.only(top: 30),
-//         color: Colors.white,
-//         child: Column(
-//         children: <Widget>[
-//           AppBar(title: Text('Contoh Cupertino')),
-//           CupertinoButton(
-//             child: Text("Contoh button"),
-//             onPressed: () {},
-//             ),
-//           CupertinoActivityIndicator(), 
-//         ],
-//         ),
-//       ),
-//     );
-//     }
-//   }
-
-// //--image widget--
-// class MyApp extends StatelessWidget {
-// @override
-//   Widget build(BuildContext context) {
-//   return MaterialApp(
-//     home:Image(image: NetworkImage('https://image.freepik.com/free-vector/cartoon-owl-tree-branch_194935-43.jpg'),
-//     ),
-//     );
-//   }
-// }
-
-//--text widget--
-// class MyApp extends StatelessWidget {
-// @override
-//   Widget build(BuildContext context) {
-//   return MaterialApp(
-//   home : Text('This is Text Widget',),
-//   ); 
-//   }
-// } 
 
 
